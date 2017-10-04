7fb0ef33033e2efd3e5d32b8968de75d813eb6c3
song <523686931@qq.com>
2017-10-04 17:31:55 +0800

    添加-protobuf相关依赖和代码测试

---
+++ src/main/java/com/test/protobuf/TeacherMessageTest.java
@@ -0,0 +1,20 @@
+package com.test.protobuf;
+
+import sun.jvm.hotspot.oops.ExceptionTableElement;
+
+public class TeacherMessageTest {
+
+    public static void main(String[] args) throws Exception{
+
+        DataInfo.Teacher teacher = DataInfo.Teacher.newBuilder().setName("卡尔大魔王").setAge(23)
+                .setAddress("qiqihaer").build();
+        byte[] teacher2ByteArray = teacher.toByteArray();
+
+        DataInfo.Teacher teacher1 = DataInfo.Teacher.parseFrom(teacher2ByteArray);
+        System.out.println(teacher1.getName());
+        System.out.println(teacher1.getAge());
+        System.out.println(teacher1.getAddress());
+
+    }
+
+}
