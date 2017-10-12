46dd95d972f2a0fe88ee5d468f2d14eef17563db
song <523686931@qq.com>
2017-10-12 20:40:30 +0800

    thrift-客户端和服务端相互调用

---
+++ src/main/java/com/test/thrift/ThriftClient.java
@@ -0,0 +1,54 @@
+package com.test.thrift;
+
+import com.test.thrift.generated.Person;
+import com.test.thrift.generated.PersonService;
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFastFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+/**
+ * 客户端
+ */
+public class ThriftClient {
+
+    public static void main(String[] args) {
+
+        TTransport transport = new TFastFramedTransport(new TSocket("localhost", 9999), 600);
+        TProtocol protocol = new TCompactProtocol(transport);
+        PersonService.Client client = new PersonService.Client(protocol);
+
+        try {
+
+            transport.open();
+
+            Person person = client.getPersonByUsername("张三");
+            System.out.println(person.getUsername());
+            System.out.println(person.getAge());
+            System.out.println(person.isMarried());
+
+            System.out.println("-----");
+
+            Person person2 = new Person();
+            person2.setUsername("魔五");
+            person2.setAge(1005);
+            person.setMarried(true);
+
+            client.savePerson(person2);
+
+        } catch (Exception ex) {
+
+            throw new RuntimeException(ex);
+
+        } finally {
+
+            transport.close();
+
+        }
+
+    }
+
+
+}
